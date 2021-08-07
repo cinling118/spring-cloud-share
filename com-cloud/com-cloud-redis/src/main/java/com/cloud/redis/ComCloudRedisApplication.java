4bb28b73fc1281be51e5945d21c70aacd3eae214
陈智云 <zhiyun.chen@autozi.com>
2021-08-07 14:17:01 +0800

    new redis project

---
+++ com-cloud/com-cloud-redis/src/main/java/com/cloud/redis/ComCloudRedisApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComCloudRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComCloudRedisApplication.class, args);
+    }
+
+}
