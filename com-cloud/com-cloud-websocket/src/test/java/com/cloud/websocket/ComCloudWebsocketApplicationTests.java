c050a0b76922e3ccc6cf1dee8632f297dd6bad49
陈智云 <zhiyun.chen@autozi.com>
2021-08-19 18:23:00 +0800

    websocket项目集成

---
+++ com-cloud/com-cloud-websocket/src/test/java/com/cloud/websocket/ComCloudWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.websocket;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComCloudWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
